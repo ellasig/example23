c7e43a581073766a25f936e68d2bc743f9b73b12
ellasig <103430174+ellasig@users.noreply.github.com>
2023-08-25 10:46:35 +0300

    ajax on progress

--- components/List.js
+++ components/List.js
@@ -1,74 +1,38 @@
 import { FlatList } from "react-native";
 import ListItem from "./ListItem";
 import {StyleSheet, SafeAreaView, Platform} from 'react-native';
+import { useEffect, useState } from "react";
+
+const url = 'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
+let mediaArray = [];
 
 
-const mediaArray = [
-  {
-    'key': '0',
-    'title': 'Title 1',
-    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
-    'thumbnails': {
-      w160: 'http://placekitten.com/160/161',
-    },
-    'filename': 'http://placekitten.com/2048/1920',
-  },
-  {
-    'key': '1',
-    'title': 'Title 2',
-    'description': 'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
-    'thumbnails': {
-      w160: 'http://placekitten.com/160/164',
-    },
-    'filename': 'http://placekitten.com/2041/1922',
-  },
-  {
-    'key': '2',
-    'title': 'Title 3',
-    'description': 'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
-    'thumbnails': {
-      w160: 'http://placekitten.com/160/167',
-    },
-    'filename': 'http://placekitten.com/2039/1920',
-  },
-];
+
+
 
 const List = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+
+  const loadMedia = async () => {
+    try {
+      const response = await fetch(url);
+      const json = response.json();
+      //console.log(json);
+      setMediaArray(json);
+    } catch (error) {
+      console.error('loadMedia failed', error);
+      }
+  };
+
+  useEffect(() => {
+    loadMedia();
+  }, []);
+
+
   return
     <FlatList data={mediaArray}
      renderItem={({item}) => <ListItem singleMedia={item}/> } />;
 };
 
 
-const styles = StyleSheet.create({
-
-  itemContainer: {
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    backgroundColor: '#f0f0f0',
-    padding: 10,
-    marginVertical: 5,
-    borderRadius: 5,
-
-  },
-  thumbnail: {
-    width: 100,
-    height: 200,
-    /*marginRight: 10,*/
-    borderRadius: 5,
-  },
-  textContainer: {
-    flex: 1,
-    flexDirection: 'column',
-    marginLeft: 10,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    /*marginBottom: 5, */
-  },
-  description: {
-    fontSize: 14,
-  },
-});
 export default List;
