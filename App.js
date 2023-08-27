73e712da7d846a80d92f73d1377a2ff45da34011
ellasig <103430174+ellasig@users.noreply.github.com>
2023-08-27 21:39:28 +0300

    -

--- App.js
+++ App.js
@@ -1,49 +1,58 @@
-import {Platform, SafeAreaView, StyleSheet} from 'react-native';
+import {Platform, SafeAreaView, StyleSheet, View, ImageBackground, Text} from 'react-native';
 import {StatusBar} from 'expo-status-bar';
 import List from './components/List';
 
 const App = () => {
+  const image = { uri: 'http://placekitten.com/2039/1920' };
   return (
-    <>
-      <SafeAreaView style={styles.container}>
-        <List />
-      </SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <ImageBackground style={styles.header} source={image}>
+          <Text style={styles.headerTitle}>Cute Cats</Text>
+        </ImageBackground>
+      </View>
+
+      <List />
       <StatusBar style="auto" />
-    </>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+
+
   container: {
-    paddingTop: Platform.OS === 'android' ? 30 : 0,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 10,
+    padding: 10,
+    backgroundColor: 'grey',
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 1,
+    shadowRadius: 3,
+    elevation: 10,
   },
+
+  header: {
+    height: 250,
+  },
+
+  headerTitle: {
+    position: 'absolute',
+    padding: 10,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+    bottom: 10,
+    backgroundColor: 'grey',
+  },
+
   TouchableOpacity: {
     marginVertical: 2,
+    padding: 5,
   },
-  box: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'lightblue',
-    borderRadius: 5
-  },
-  Image: {
-    margin: 10,
-    width: '45%',
-    height: 200,
-    borderRadius: 5
 
-  },
-  View: {
-    width: '50%',
-    margin: 5,
-  },
-  Text: {
-    height: 'auto',
-    width: '100%',
-  },
 });
 
 export default App;
+
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,51 +1,98 @@
 import React from 'react';
-import { Image, Text, TouchableOpacity, View, StyleSheet } from 'react-native';
+import { Image, Text, TouchableOpacity, View, ImageBackground, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 
 const ListItem = (props) => {
   return (
-    <TouchableOpacity style={styles.TouchableOpacity}>
-      <View style={styles.box}>
-        <Image
-          style={styles.Image}
-          source={{ uri: props.singleMedia.thumbnails.w160 }}
-        />
-        <View style={styles.View}>
-          <Text style={styles.Title}>{props.singleMedia.title}</Text>
-          <Text>{props.singleMedia.description}</Text>
-        </View>
+
+      <View style={styles.container}>
+
+
+      <TouchableOpacity style={styles.TouchableOpacity}>
+        <View style={styles.box}>
+          <Image
+            style={styles.Image}
+            source={{ uri: props.singleMedia.thumbnails.w160 }}
+          />
+          <View style={styles.View}>
+            <Text style={styles.Title}>{props.singleMedia.title}</Text>
+            <Text style={styles.Description}>{props.singleMedia.description}</Text>
+          </View>
+          </View>
+      </TouchableOpacity>
       </View>
-    </TouchableOpacity>
   );
 };
+
 
 ListItem.propTypes = {
   singleMedia: PropTypes.object,
 };
 
 const styles = StyleSheet.create({
+
+
+  container: {
+    margin: 10,
+    padding: 10,
+    backgroundColor: 'grey',
+    borderRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 1,
+    shadowRadius: 3,
+    elevation: 10,
+  },
+
+  header: {
+    height: 200,
+    //justifyContent: 'center',
+    //alignItems: 'center'
+  },
+
+  headerTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  
   TouchableOpacity: {
     marginVertical: 2,
+    padding: 5,
   },
+
   box: {
-    flex: 1,
     flexDirection: 'row',
-    backgroundColor: 'lightblue',
-    borderRadius: 5,
+    borderRadius: 15,
+    margin: 10,
+    padding: 10,
+    justifyContent: 'center'
   },
   Image: {
-    margin: 10,
     width: '45%',
-    height: 200,
-    borderRadius: 5,
+    height: 250,
+    borderRadius: 40,
+
   },
+
   View: {
-    width: '50%',
-    margin: 5,
+    width: '60%',
+    marginLeft: 20,
   },
   Title: {
     fontSize: 18,
     fontWeight: 'bold',
+    fontStyle: 'italic',
+
+  },
+  Description: {
+    marginTop: 5,
+    marginBottom: 5,
+    fontStyle: 'italic',
+    margin: 5,
+    color: 'white'
+
+
   },
 });
 
