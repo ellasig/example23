0f322e682afb581fcc0ffe84d7ae7ccf8585983c
ellasig <103430174+ellasig@users.noreply.github.com>
2023-09-01 14:57:15 +0300

    login works

--- views/Login.js
+++ views/Login.js
@@ -9,11 +9,11 @@ import PropTypes from 'prop-types';
 import { MainContext } from '../contexts/MainContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useAuthentication, useUser } from '../hooks/ApiHooks';
+import LoginForm from '../components/LoginForm';
 
 const Login = ({navigation}) => {
   // props is needed for navigation
 const {setIsLoggedIn} = useContext(MainContext);
-const {postLogin} = useAuthentication();
 const {getUserByToken} = useUser();
 
 
@@ -35,27 +35,11 @@ useEffect(() => {
   checkToken();
 }, []);
 
-const logIn = async () => {
-      console.log('Login button pressed');
-      try {
-        const loginResponse = await postLogin({
-        username: 'ella',
-        password: 'salasana',
-      });
-      console.log('login response', loginResponse);
-      // TODO: fix dofetch() to display errors from API (e.g when bad user/pw)
-      // use loginResponse.user for storing token & userdata
-        await AsyncStorage.setItem('userToken', loginResponse.token);
-        setIsLoggedIn(true);
-      } catch(error) {
-        console.error(error);
-        //TODO: notify user about failed login?
-      }
-  };
+
   return (
     <View style={styles.container}>
       <Text>Login</Text>
-      <Button title="Sign in!" onPress={logIn}/>
+      <LoginForm/>
     </View>
   );
 };
